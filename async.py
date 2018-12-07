01d6bd99eb7db860ea67a688fdd6f61f9dee0a28
Evan West <etwest@ucsc.edu>
2018-12-06 16:30:36 -0800

    async broadcast

---
+++ async.py
@@ -0,0 +1,22 @@
+import requests
+import asyncio
+import concurrent.futures
+import time
+
+def send_broadcast(urls):
+	answer = []
+	size = len(urls)
+	async def broadcast():
+		with concurrent.futures.ThreadPoolExecutor(max_workers=size%30) as executor:
+			loop = asyncio.get_event_loop()
+			futures = [0 for _ in range(size)]
+			for i in range(size):
+				print(urls[i])
+				futures[i] = loop.run_in_executor(executor, requests.get, urls[i])
+			for response in await asyncio.gather(*futures):
+				answer.append(response)	
+	
+	loop = asyncio.get_event_loop()
+	loop.run_until_complete(broadcast())
+	return answer
+
