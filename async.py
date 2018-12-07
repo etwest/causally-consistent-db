d188dae21f44f7a14bf0556417bc30414fd40394
Evan West <etwest@ucsc.edu>
2018-12-07 01:59:15 -0800

    async broadcast working

--- async.py
+++ async.py
@@ -1,38 +1,111 @@
 import requests
-import asyncio
-import concurrent.futures
 import time
+from threading import Thread
 
-def send_get_broadcast(urls):
+class Get_thread(Thread):
+	def __init__(self, url, timeout):
+		Thread.__init__(self)
+		self.result = None
+		self.failed = False
+		self.daemon = True
+		self.url = url
+		self.timeout = timeout
+
+	def run(self):
+		tries = 0
+		r = None
+		while tries < 3:
+			try:
+				r = requests.get(self.url, timeout=self.timeout)
+				self.result = r 
+				return
+			except(requests.HTTPError, requests.ConnectionError, requests.Timeout):
+				tries += 1
+		self.failed = True
+
+class Put_thread(Thread):
+	def __init__(self, url, timeout):
+		Thread.__init__(self)
+		self.result = None
+		self.failed = False
+		self.daemon = True
+		self.url = url
+		self.timeout = timeout
+
+	def run(self):
+		tries = 0
+		r = None
+		while tries < 3:
+			try:
+				r = requests.put(self.url, timeout=self.timeout)
+				self.result = r
+				return
+			except(requests.HTTPError, requests.ConnectionError, requests.Timeout):
+				tries += 1
+		self.failed = True
+
+#####################################################
+############# Functions to actually use #############
+#####################################################
+
+# urls is a list of the urls to query and timeout is a float number
+#	which is hold long to wait for a response in seconds before retrying
+
+# returns a list of either False (if the request failed) or a response object
+def get_broadcast(urls, timeout):
 	answer = []
-	size = len(urls)
-	async def broadcast():
-		with concurrent.futures.ThreadPoolExecutor(max_workers=size%30) as executor:
-			loop = asyncio.get_event_loop()
-			futures = [0 for _ in range(size)]
-			for i in range(size):
-				print(urls[i])
-				futures[i] = loop.run_in_executor(executor, requests.get, urls[i], timeout=.5)
-			for response in await asyncio.gather(*futures):
-				answer.append(response)	
-	
-	loop = asyncio.get_event_loop()
-	loop.run_until_complete(broadcast())
+	threads = [
+		Get_thread(url, timeout) for url in urls
+	]
+	for i in range(len(urls)):
+		threads[i].start()
+	for i in range(len(urls)):
+		threads[i].join()
+		if not threads[i].failed:
+			answer.append(threads[i].result)
+		else:
+			answer.append(False)
 	return answer
 
-def send_put_broadcast(urls):
+# urls is a list of the urls to query and timeout is a float number 
+#	which is hold long to wait for a response in seconds before retrying
+
+# returns a list of either False (if the request failed) or a response object
+def put_broadcast(urls, timeout):
 	answer = []
-	size = len(urls)
-	async def broadcast():
-		with concurrent.futures.ThreadPoolExecutor(max_workers=size%30) as executor:
-			loop = asyncio.get_event_loop()
-			futures = [0 for _ in range(size)]
-			for i in range(size):
-				print(urls[i])
-				futures[i] = loop.run_in_executor(executor, requests.put, urls[i], timeout=.5)
-			for response in await asyncio.gather(*futures):
-				answer.append(response)	
-	
-	loop = asyncio.get_event_loop()
-	loop.run_until_complete(broadcast())
+	threads = [
+		Put_thread(url, timeout) for url in urls
+	]
+	for i in range(len(urls)):
+		threads[i].start()
+	for i in range(len(urls)):
+		threads[i].join()
+		if not threads[i].failed:
+			answer.append(threads[i].result)
+		else:
+			answer.append(False)
 	return answer
+
+
+# Example of using these functions to send a broadcast of 20 messages to example.com (GET and PUT)
+
+# urls = ['http://192.123.123.12/'] #a url that should fail to connect
+# for _ in range(20):
+# 	urls.append('http://www.example.com/')
+# curr = time.time()
+# responses = get_broadcast(urls, .5)
+# print(time.time() - curr)
+# for r in responses:
+# 	if r != False:
+# 		print(r.text)
+# 	else:
+# 		print(False)
+
+# curr = time.time()
+# responses = put_broadcast(['http://www.example.com' for _ in range(20)], .5)
+# print(time.time() - curr)
+# for r in responses:
+# 	if r != False:
+# 		print(r.text)
+# 	else:
+# 		print(False)
