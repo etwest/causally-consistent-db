dd63546e99b565c9c5b2a02bcdacf13c6225dbe1
Bryan Ji <xj9797@gmail.com>
2018-12-06 18:47:35 -0800

    working on async

--- async.py
+++ async.py
@@ -3,7 +3,7 @@ import asyncio
 import concurrent.futures
 import time
 
-def send_broadcast(urls):
+def send_get_broadcast(urls):
 	answer = []
 	size = len(urls)
 	async def broadcast():
@@ -12,7 +12,7 @@ def send_broadcast(urls):
 			futures = [0 for _ in range(size)]
 			for i in range(size):
 				print(urls[i])
-				futures[i] = loop.run_in_executor(executor, requests.get, urls[i])
+				futures[i] = loop.run_in_executor(executor, requests.get, urls[i], timeout=.5)
 			for response in await asyncio.gather(*futures):
 				answer.append(response)	
 	
@@ -20,3 +20,19 @@ def send_broadcast(urls):
 	loop.run_until_complete(broadcast())
 	return answer
 
+def send_put_broadcast(urls):
+	answer = []
+	size = len(urls)
+	async def broadcast():
+		with concurrent.futures.ThreadPoolExecutor(max_workers=size%30) as executor:
+			loop = asyncio.get_event_loop()
+			futures = [0 for _ in range(size)]
+			for i in range(size):
+				print(urls[i])
+				futures[i] = loop.run_in_executor(executor, requests.put, urls[i], timeout=.5)
+			for response in await asyncio.gather(*futures):
+				answer.append(response)	
+	
+	loop = asyncio.get_event_loop()
+	loop.run_until_complete(broadcast())
+	return answer
